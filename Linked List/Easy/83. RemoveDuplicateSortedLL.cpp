3504a783102c7c1cd9c2fda90d0826b40eb2573c
Subrat Kumar Singh <92250219+SubratKumarSingh2001@users.noreply.github.com>
2025-02-18 20:54:36 +0530

    Create 83. RemoveDuplicateSortedLL.cpp

---
+++ Linked List/Easy/83. RemoveDuplicateSortedLL.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        //TC: O(n) where n is no.of nodes in LL
+        //SC: O(1)
+        ListNode* temp = head;
+        while(temp != NULL && temp->next != NULL) {
+            if(temp->val != temp->next->val) {
+                temp = temp->next;
+            }
+            else {
+                ListNode* curr = temp->next;
+                if(curr != NULL) {
+                    ListNode* forward = curr->next;
+                    temp->next = forward;
+                    curr->next = NULL;
+                    delete curr;
+                }
+            }
+        }
+        return head;
+    }
+};
