35620593e32d5cee407042d9c68762844df3d9cd
Subrat Kumar Singh <92250219+SubratKumarSingh2001@users.noreply.github.com>
2025-02-22 22:10:29 +0530

    Create 367.ValidPerfectSquare.cpp

---
+++ Math/Easy/367.ValidPerfectSquare.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        //TC: O(logn), SC: O(1)
+        int start = 0, end = num;
+        /* here we had use long long because when we multiply large mid value with
+        same mid value it may go out of integer rang so we have use long long of 8 
+        bytes  i.e 64 bits which can store the mid*mid large valu considering the 
+        integer range 
+        */
+        long long mid = start + (end-start)/2;
+        while(start <= end) {
+            if(mid*mid == num) {
+                return true;
+            }
+            else if(mid*mid > num) {
+                end = mid-1;
+            }
+            else {
+                start = mid+1;
+            }
+            mid = start + (end-start)/2;
+        }
+        return false;
+    }
+};
