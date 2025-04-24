58a242714ace94c87d3e7cc0f2aff29a9ba33fc7
Subrat Kumar Singh <92250219+SubratKumarSingh2001@users.noreply.github.com>
2025-04-24 10:45:45 +0530

    Update 19.RemoveNthNodeEnd.cpp

--- Linked List/Medium/19.RemoveNthNodeEnd.cpp
+++ Linked List/Medium/19.RemoveNthNodeEnd.cpp
@@ -1,42 +1,68 @@
 class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {\
-        //TC: O(n) where n is number of nodes in Linked List
-        if(head->next == NULL) {
-            delete head; 
-            return NULL;
-        }
-
+    int cntNodes(ListNode* head) {
+        int cnt=0;
         ListNode* temp = head;
-        int count=0;
-        while(temp != NULL) {
-            count++;
+        while(temp) {
+            cnt++;
             temp = temp->next;
         }
+        return cnt;
+    }
 
-        if(count == n) {
-            ListNode* temp = head;
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        //if the node delete is the first node 
+        //Brute Force
+        //TC: O(len)+O(len-n) for worse n=1(delete last node), Therfore TC: O(2*len)
+        //SC: O(1)
+        int cnt = cntNodes(head); //cnt is number of nodes in linked list
+        if(cnt == n) {
             head = head->next;
-            temp->next = NULL;
-            delete temp;
             return head;
         }
 
+        //for value of n>1, code below will be the logic
         int i=1;
-        temp = head;
-        while(i<count-n) {
+        ListNode* temp = head;
+        while(i < cnt-n) {
             temp = temp->next;
             i++;
         }
-        ListNode* target = temp->next;
-        ListNode* forward = target->next;
-        temp->next = forward;
-        target->next = NULL;
-        delete target;
+        ListNode* forward = temp->next;
+        temp->next = forward->next;
 
         return head;
 
-        //Optimal Way: Two pointers i.e Slow and Fast Pointers (Tortoise and Hare)
-        //will do it later..........
+        //Optimal Approach
+        //TC: O(n), n is lenght of the nodes, SC: O(1)
+        /*We have use slow and fast pointers as we need to delete the nth node from the 
+        last which means we need a pointer that points the previous node of the node that
+        we want to delete, if we want to delete the nth node from last wich means we need
+        to move ahead the len-n nodes from beginning for which we dont need the no. of
+        nodes in Linked List we will traverse the pointer until n times which will autom-
+        -atically help us to move ahead len-n nodes, so to reach on prev node of delete
+        node move two pointers i.e slow anf fast one node ahead until fast->next != NULL
+        . Its basically a mathematical obseravtion
+        */
+        ListNode* fast = head;
+        for(int i=0; i<n; i++) {
+            fast = fast->next;
+        }
+        if(fast == NULL) { //when n=len of the linked list
+            head = head->next;
+            return head;
+        }
+        ListNode* slow = head;
+        while(fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        //now we have to delete the node
+        ListNode* temp = slow->next;
+        slow->next = temp->next;
+        temp->next = NULL;
+        delete(temp);
+
+        return head;
     }
 };
