4d24fcb58b8a002c8d369ee6716c7eb0aa411f0c
Subrat Kumar Singh <92250219+SubratKumarSingh2001@users.noreply.github.com>
2025-07-07 21:43:03 +0530

    Update 2540.MiniCommonValue.cpp

--- Binary-Search/Easy/2540.MiniCommonValue.cpp
+++ Binary-Search/Easy/2540.MiniCommonValue.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        //Two Pointers Approach, TC: O(n), SC: O(1)
+        //Two Pointers Approach, TC: O(n+m), SC: O(1)
         int i=0, j=0;
         while(i<nums1.size() && j<nums2.size()) {
             if(nums1[i] == nums2[j]) {
